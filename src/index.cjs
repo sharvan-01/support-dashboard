804435743cb3b900bf126394ddbe9b5d74fd3fbf
Sharvan TG <sharvan@plumhq.com>
2023-12-11 15:53:07 +0530

    New design

--- src/index.cjs
+++ src/index.cjs
@@ -32,6 +32,7 @@ async function createData() {
         await formatData(value);
         if (key === 'MFRT') {
           await chart1(fcrLabels, fcrValues);
+          await chart4(fcrLabels, fcrValues);
         } else if (key === 'CSAT') {
           console.log(fcrValues);
           var inc = 0;
@@ -40,8 +41,10 @@ async function createData() {
             inc++;
           });
           await chart2(fcrLabels, fcrValues);
+          chart5(fcrLabels, fcrValues);
         } else {
           await chart3(fcrLabels, fcrValues);
+          chart6(fcrLabels, fcrValues);
         }
       }
     }
@@ -68,7 +71,16 @@ async function formatData(object) {
 var data = {};
 var fcrLabels = [];
 var fcrValues = [];
+var ctx = document.getElementById('mfrt').getContext('2d');
+var gradient = ctx.createLinearGradient(0, 0, 0, 400);
+gradient.addColorStop(0, 'rgba(87,14,64,1)');
+gradient.addColorStop(1, 'rgba(188,158,177,0.3)');
+var gradient2 = ctx.createLinearGradient(0, 0, 0, 400);
+gradient2.addColorStop(0, 'rgba(87,14,64,1)');
+gradient2.addColorStop(1, 'rgba(188,158,177,0)');
 createData();
+
+// https://uploads-ssl.webflow.com/63413ad4dec8a91fe53f4fb0/63413ad4dec8a930443f4fd3_Chromatica-Regular.woff2
 
 async function chart1(label, value) {
   new Chart(document.getElementById('mfrt'), {
@@ -80,11 +92,17 @@ async function chart1(label, value) {
           borderWidth: '2',
           label: '',
           data: value,
-          fill: false,
+          fill: {
+            target: 'origin',
+            // below: 'rgb(0, 0, 255)',
+            above: gradient,
+          },
+
           //borderColor: 'rgb(75, 192, 192)',
           tension: 0.2,
           backgroundColor: '#FFFAF2',
-          borderColor: '#FF4052',
+          borderColor: '#340926',
+          width: 2,
         },
       ],
     },
@@ -94,25 +112,55 @@ async function chart1(label, value) {
           grid: {
             display: false,
           },
+          border: {
+            width: 1,
+            color: '#340926',
+          },
           title: {
+            family: 'Chromatica',
             text: 'Date',
             display: true,
+            color: '#570E40',
+            font: {
+              family: 'Chromatica',
+              size: 20,
+              color: '#570E40',
+            },
           },
           ticks: {
-            fontSize: 40,
+            maxTicksLimit: 6,
+            padding: 10,
+            color: '#570E40',
+          },
+          font: {
+            family: 'Chromatica',
           },
         },
         y: {
           grid: {
             display: false,
           },
+          border: {
+            width: 1,
+            color: '#340926',
+          },
           title: {
-            text: 'Median First Response Time in Hours',
+            text: 'Hours',
             display: true,
-            fontSize: 30,
+            color: '#570E40',
+            font: {
+              family: 'Chromatica',
+              size: 20,
+              color: '#570E40',
+            },
           },
           ticks: {
-            fontSize: 30,
+            maxTicksLimit: 6,
+            padding: 10,
+            color: '#570E40',
+            font: {
+              family: 'Chromatica',
+            },
           },
         },
       },
@@ -135,32 +183,74 @@ async function chart2(label, value) {
           borderWidth: '2',
           label: '',
           data: value,
-          fill: false,
+          fill: {
+            target: 'origin',
+            // below: 'rgb(0, 0, 255)',
+            above: gradient,
+          },
+
           //borderColor: 'rgb(75, 192, 192)',
           tension: 0.2,
           backgroundColor: '#FFFAF2',
-          borderColor: '#FF4052',
+          borderColor: '#340926',
+          width: 2,
         },
       ],
     },
     options: {
       scales: {
         x: {
-          title: {
-            text: 'Date',
-            display: true,
-          },
           grid: {
             display: false,
+          },
+          border: {
+            width: 1,
+            color: '#340926',
+          },
+          title: {
+            family: 'Chromatica',
+            text: 'Date',
+            display: true,
+            color: '#570E40',
+            font: {
+              family: 'Chromatica',
+              size: 20,
+              color: '#570E40',
+            },
+          },
+          ticks: {
+            maxTicksLimit: 6,
+            padding: 10,
+            color: '#570E40',
+          },
+          font: {
+            family: 'Chromatica',
           },
         },
         y: {
-          title: {
-            text: 'Percentage',
-            display: true,
-          },
           grid: {
             display: false,
+          },
+          border: {
+            width: 1,
+            color: '#340926',
+          },
+          title: {
+            text: 'Hours',
+            display: true,
+            color: '#570E40',
+            font: {
+              family: 'Chromatica',
+              size: 20,
+              color: '#570E40',
+            },
+          },
+          ticks: {
+            // maxTicksLimit: 6,
+            family: 'Chromatica',
+            beginAtZero: true,
+            stepSize: 20,
+            max: 100,
           },
         },
       },
@@ -182,32 +272,236 @@ async function chart3(label, value) {
           borderWidth: '2',
           label: '',
           data: value,
-          fill: false,
+          fill: {
+            target: 'origin',
+            // below: 'rgb(0, 0, 255)',
+            above: gradient,
+          },
+
           //borderColor: 'rgb(75, 192, 192)',
           tension: 0.2,
           backgroundColor: '#FFFAF2',
-          borderColor: '#FF4052',
+          borderColor: '#340926',
+          width: 2,
         },
       ],
     },
     options: {
       scales: {
         x: {
-          title: {
-            text: 'Date',
-            display: true,
-          },
           grid: {
             display: false,
+          },
+          border: {
+            width: 1,
+            color: '#340926',
+          },
+          title: {
+            family: 'Chromatica',
+            text: 'Date',
+            display: true,
+            color: '#570E40',
+            font: {
+              family: 'Chromatica',
+              size: 20,
+              color: '#570E40',
+            },
+          },
+          ticks: {
+            maxTicksLimit: 6,
+            padding: 10,
+            color: '#570E40',
+          },
+          font: {
+            family: 'Chromatica',
           },
         },
         y: {
-          title: {
-            text: 'First Call Resolution (in hours)',
-            display: true,
-          },
           grid: {
             display: false,
+          },
+          border: {
+            width: 1,
+            color: '#340926',
+          },
+          title: {
+            text: 'Hours',
+            display: true,
+            color: '#570E40',
+            font: {
+              family: 'Chromatica',
+              size: 20,
+              color: '#570E40',
+            },
+          },
+          ticks: {
+            maxTicksLimit: 6,
+            padding: 10,
+            color: '#570E40',
+            font: {
+              family: 'Chromatica',
+            },
+          },
+        },
+      },
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+    },
+  });
+}
+
+async function chart4(label, value) {
+  new Chart(document.getElementById('mfrt-hero'), {
+    type: 'line',
+    data: {
+      labels: label,
+      datasets: [
+        {
+          label: false,
+          borderWidth: 2,
+
+          data: value,
+          fill: {
+            target: 'origin',
+            // below: 'rgb(0, 0, 255)',
+            above: gradient2,
+          },
+
+          //borderColor: 'rgb(75, 192, 192)',
+          tension: 0.2,
+          backgroundColor: '#FFFAF2',
+          borderColor: '#340926',
+        },
+      ],
+    },
+    options: {
+      scales: {
+        x: {
+          display: false,
+          grid: {
+            display: false,
+            drawOnChartArea: false,
+            drawTicks: false,
+            color: 'transparent',
+            width: 0,
+          },
+          border: {
+            display: false,
+          },
+          ticks: {
+            display: false,
+            padding: 0,
+          },
+        },
+        y: {
+          display: false,
+          grid: {
+            display: false,
+            drawOnChartArea: false,
+            drawTicks: false,
+            color: 'transparent',
+            width: 0,
+          },
+          border: {
+            display: false,
+          },
+          ticks: {
+            backdropPadding: 0,
+            display: false,
+            mirror: true,
+            padding: 0,
+          },
+        },
+      },
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+    },
+  });
+}
+
+async function chart5(label, value) {
+  new Chart(document.getElementById('csat-hero'), {
+    type: 'line',
+    data: {
+      labels: label,
+      datasets: [
+        {
+          label: '',
+          data: value,
+          fill: {
+            target: 'origin',
+            // below: 'rgb(0, 0, 255)',
+            above: gradient,
+          },
+
+          //borderColor: 'rgb(75, 192, 192)',
+          tension: 0.2,
+          backgroundColor: '#FFFAF2',
+          borderColor: '#340926',
+          width: 2,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        x: {
+          display: false,
+        },
+        y: {
+          display: false,
+        },
+      },
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+    },
+  });
+}
+
+async function chart6(label, value) {
+  new Chart(document.getElementById('fcr-hero'), {
+    type: 'line',
+    data: {
+      labels: label,
+      datasets: [
+        {
+          label: '',
+          data: value,
+          fill: {
+            target: 'origin',
+            // below: 'rgb(0, 0, 255)',
+            above: gradient,
+          },
+
+          //borderColor: 'rgb(75, 192, 192)',
+          tension: 0.2,
+          backgroundColor: '#FFFAF2',
+          borderColor: '#340926',
+          width: 2,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        x: {
+          display: false,
+          grid: {
+            drawTicks: false,
+          },
+        },
+
+        y: {
+          display: false,
+          grid: {
+            drawTicks: false,
           },
         },
       },
